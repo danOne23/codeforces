1de7e8e7264317ee99cdb0cfe5e78fa04da6e72f
danOne23 <danonemun@gmail.com>
2020-09-30 21:58:10 +0200

    Solved

---
+++ candies.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int t;
+    cin>>t;
+
+    long long r [t];
+
+    for (int i = 0; i < t; i++)
+    {
+        long long n;
+        cin>>n;
+    }
+    
+
+}
