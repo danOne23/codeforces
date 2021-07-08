85ed0e290e301e8635c33ff37fad7452fa319e29
Danyal <danonemun@gmail.com>
2021-07-08 20:33:08 +0200

    sovled

--- petyaandstrings.cpp
+++ petyaandstrings.cpp
@@ -3,78 +3,21 @@ using namespace std;
 
 int main()
 {
+	string s1, s2;
+	cin >> s1 >> s2;
 
-	vector<string> s;
-	vector<int> sum{0, 0};
-	int n;
-
-	for (int i = 0; i < 2; i++)
+	for (int i = 0; i < s1.length(); i++)
 	{
-		string str;
-		cin >> str;
-		sort(str.begin(), str.end());
-		n = str.length();
-
-		for (int k = 0; k < str.length(); k++)
-		{
-			for (int l = 0; l < n; l++)
-			{
-				for (int j = i + 1; j < n; j++)
-				{
-					if (str[i] > str[j])
-					{
-						int temp = str[i];
-						str[i] = str[j];
-						str[j] = temp;
-					}
-				}
-			}
-		}
-
-		s.push_back(str);
+		s1[i] = tolower(s1[i]);
+		s2[i] = tolower(s2[i]);
 	}
 
-	n = s[0].length();
-	map<char, int> values = {
-		{'a', 1},
-		{'b', 2},
-		{'c', 3},
-		{'d', 4},
-		{'e', 5},
-		{'f', 6},
-		{'g', 7},
-		{'h', 8},
-		{'j', 9},
-		{'j', 10},
-		{'k', 11},
-		{'l', 12},
-		{'m', 13},
-		{'n', 14},
-		{'o', 15},
-		{'p', 16},
-		{'q', 17},
-		{'r', 18},
-		{'s', 19},
-		{'t', 20},
-		{'u', 21},
-		{'v', 22},
-		{'w', 23},
-		{'x', 24},
-		{'y', 25},
-		{'z', 26}};
-
-	for (int i = 0; i < 2; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			sum[i] += values[tolower(s[i][j])];
-		}
-	}
-
-	if (sum[0] > sum[1])
-		cout << 1 << endl;
-	else if (sum[0] < sum[1])
-		cout << -1 << endl;
+	if (s1 > s2)
+		cout
+			<< 1 << endl;
+	else if (s1 < s2)
+		cout
+			<< -1 << endl;
 	else
 		cout << 0 << endl;
 }
